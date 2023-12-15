3158a9a75114719dafaa4e70e963120b4d9b60d7
Piyush Pardeshi <piyushp0512@gmail.com>
2023-12-15 19:12:41 +0530

    idk

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,11 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  
+  return (
+    <div className="App">
+      <p>Test</p>
+    </div>
+  );
 }
 
 export default App;
