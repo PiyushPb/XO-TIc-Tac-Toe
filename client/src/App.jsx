f76b9ab266fbb6e046aba9698bc101fca4d82c78
Piyush Pardeshi <piyushp0512@gmail.com>
2023-12-15 19:20:38 +0530

    Browser router code added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from "react";
 import "./App.css";
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+
+import Home from "./page/Home";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <p>Test</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
