28d034d6271d756f7a6eac9aea235cc725cc34c9
Pranav <pranav.shastri04@gmail.com>
2023-12-15 19:02:41 +0530

    mava

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <h1 className="text-3xl font-bold underline">Hello!</h1>;
+  return <h1 className="text-3xl font-bold underline">mava!</h1>;
 }
 
 export default App;
