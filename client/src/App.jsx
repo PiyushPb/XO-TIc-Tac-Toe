751d5ce6871d73172be774d166785cf804b15c4f
Piyush Pardeshi <piyushp0512@gmail.com>
2023-12-15 19:39:18 +0530

    app.css updated, error.jsx page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import {
 } from "react-router-dom";
 
 import Home from "./page/Home";
+import Error from "./page/Error";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,6 +18,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
   );
